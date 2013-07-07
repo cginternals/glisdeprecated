de90565a47a69c98493c1b248a20a67d3107170b
cgcostume <daniel.limberger@hpi.uni-potsdam.de>
2013-07-07 13:34:33 +0200

    use google xml pretty print for now

--- htdocs/glquery.php
+++ htdocs/glquery.php
@@ -1,5 +1,44 @@
 <?php
 
+	/** Prettifies an XML string into a human-readable and indented work of art
+	 *  @param string $xml The XML as a string
+	 *  @param boolean $html_output True if the output should be escaped (for use in HTML)
+	 */
+	// http://gdatatips.blogspot.de/2008/11/xml-php-pretty-printer.html
+	function xmlpp($xml, $html_output=false) {
+	    $xml_obj = new SimpleXMLElement($xml);
+	    $level = 4;
+	    $indent = 0; // current indentation level
+	    $pretty = array();
+	    
+	    // get an array containing each XML element
+	    $xml = explode("\n", preg_replace('/>\s*</', ">\n<", $xml_obj->asXML()));
+
+	    // shift off opening XML tag if present
+	    if (count($xml) && preg_match('/^<\?\s*xml/', $xml[0])) {
+	      $pretty[] = array_shift($xml);
+	    }
+
+	    foreach ($xml as $el) {
+	      if (preg_match('/^<([\w])+[^>\/]*>$/U', $el)) {
+	          // opening tag, increase indent
+	          $pretty[] = str_repeat(' ', $indent) . $el;
+	          $indent += $level;
+	      } else {
+	        if (preg_match('/^<\/.+>$/', $el)) {            
+	          $indent -= $level;  // closing tag, decrease indent
+	        }
+	        if ($indent < 0) {
+	          $indent += $level;
+	        }
+	        $pretty[] = str_repeat(' ', $indent) . $el;
+	      }
+	    }   
+	    $xml = implode("\n", $pretty);   
+	    return ($html_output) ? htmlentities($xml) : $xml;
+	}
+
+
 	function createAndAttachNode(&$document, $tag, &$parent = nil)
 	{
 		if(nil == $document)
@@ -173,12 +212,13 @@
 	ini_set('display_errors', 0);
 
 
-	$xsl = 'href="glresult.xsl" type="text/xsl"';  
-	$stylesheet = new DOMProcessingInstruction('xml-stylesheet',$xsl);
+//	$xsl = 'href="glresult.xsl" type="text/xsl"';  
+//	$stylesheet = new DOMProcessingInstruction('xml-stylesheet',$xsl);
 
 	$result = new DOMDocument('1.0');
+	$result->preserveWhiteSpace = false;
 	$result->formatOutput = true;
-	$result->appendChild($stylesheet); 
+//	$result->appendChild($stylesheet); 
 
 	$query = value("query", "");
 	if(empty($query))
@@ -199,7 +239,9 @@
 	queryCommand($glxpath, $result, $glquery, $query);
 	queryEnum($glxpath, $result, $glquery, $query);
 
-	echo $result->saveXML();
+
+	echo xmlpp($result->saveXML(), false);
+	//echo $result->saveXML();
 
 
 
